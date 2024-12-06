c484d495c43af66075dd9d6fb3701573a5a4ebb1
Riyan Cyriac Jose <lul42xex@rhrk.uni-kl.de>
2024-12-06 05:25:43 +0100

    [ADD] Param file passing as launch configuration

--- costmap_converter/launch/standalone_converter.launch.py
+++ costmap_converter/launch/standalone_converter.launch.py
@@ -8,10 +8,7 @@ from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
 
     # Get the package directories
-    fleet_share = get_package_share_directory('costmap_converter')
-
-    # Load all parameters
-    standalone_converter_config = os.path.join(fleet_share, 'config', 'standalone_converter.yaml')
+    costmap_converter_share = get_package_share_directory('costmap_converter')
 
     # Declare Launch Arguments
     declare_namespace_cmd = DeclareLaunchArgument(
@@ -19,17 +16,23 @@ def generate_launch_description():
         default_value='',
         description='Top-level namespace')
     
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(costmap_converter_share, 'config', 'standalone_converter.yaml'),
+        description='Full path to the ROS2 parameters file')
+    
     # Setting Launch Configuration
     namespace = LaunchConfiguration('namespace')
+    params_file = LaunchConfiguration('params_file')
 
 
     # Description of Nodes
     costmap_converter_node = Node(
             package ='costmap_converter',
             executable ='standalone_converter',
-            # namespace = namespace,
+            namespace = namespace,
             output = 'screen',
-            parameters = [standalone_converter_config],
+            parameters = [params_file],
             remappings=[
                 ('/tf', 'tf'),
                 ('/tf_static', 'tf_static')],
@@ -38,6 +41,7 @@ def generate_launch_description():
     # Launch Description
     ld = LaunchDescription()
     ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_params_file_cmd)
     ld.add_action(costmap_converter_node)
     return ld
 
